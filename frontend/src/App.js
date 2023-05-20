999f664b8224ec7c168a48ca4db8ce25a1ca6922
Apoorva <apoorvapriya2001@gmail.com>
2023-05-21 01:14:38 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
