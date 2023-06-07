ce4ecb67459d9cdb8ef9cb76eddde3b03e6dafb2
Apoorva <apoorvapriya2001@gmail.com>
2023-06-07 10:33:18 +0530

    updated the about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function About() {
     return (
@@ -16,7 +17,7 @@ function About() {
                             aria-expanded="true"
                             aria-controls="collapseOne"
                         >
-                            Accordion Item #1
+                            Welcome to Stock Portfolio Management System!
                         </button>
                     </h2>
                     <div
@@ -25,13 +26,8 @@ function About() {
                         data-bs-parent="#accordionExample"
                     >
                         <div className="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by
-                            default, until the collapse plugin adds the appropriate classes that we
-                            use to style each element. These classes control the overall appearance,
-                            as well as the showing and hiding via CSS transitions. You can modify
-                            any of this with custom CSS or overriding our default variables. It's
-                            also worth noting that just about any HTML can go within the{" "}
-                            <code>.accordion-body</code>, though the transition does limit overflow.
+                            We understand the importance of managing your investments effectively. Our Stock Portfolio Management System is designed to empower you with the tools and insights you need to make informed decisions and maximize your portfolio's potential.
+                            With our user-friendly web application, you can conveniently monitor and analyze your stock holdings, track performance, and stay updated on market trends. Whether you're a seasoned investor or just starting your journey, our platform caters to all levels of expertise.
                         </div>
                     </div>
                 </div>
@@ -45,7 +41,7 @@ function About() {
                             aria-expanded="false"
                             aria-controls="collapseTwo"
                         >
-                            Accordion Item #2
+                            Join us
                         </button>
                     </h2>
                     <div
@@ -54,14 +50,8 @@ function About() {
                         data-bs-parent="#accordionExample"
                     >
                         <div className="accordion-body">
-                            <strong>This is the second item's accordion body.</strong> It is hidden
-                            by default, until the collapse plugin adds the appropriate classes that
-                            we use to style each element. These classes control the overall
-                            appearance, as well as the showing and hiding via CSS transitions. You
-                            can modify any of this with custom CSS or overriding our default
-                            variables. It's also worth noting that just about any HTML can go within
-                            the <code>.accordion-body</code>, though the transition does limit
-                            overflow.
+                            We are committed to empowering investors with the tools and resources they need to succeed in today's dynamic stock market. Join us and take control of your investments with confidence. Start exploring our Stock Portfolio Management System today!
+                            Head over to the <Link to="/register">signup</Link> page to register or <Link to="/login">login</Link> to continue your journey.
                         </div>
                     </div>
                 </div>
@@ -75,11 +65,34 @@ function About() {
                             aria-expanded="false"
                             aria-controls="collapseThree"
                         >
-                            Contact Us
+                            Disclaimer
                         </button>
                     </h2>
                     <div
                         id="collapseThree"
+                        className="accordion-collapse collapse"
+                        data-bs-parent="#accordionExample"
+                    >
+                        <div className="accordion-body">
+                            Our Stock Portfolio Management System provides tools and information for educational and informational purposes only. It does not constitute financial advice or a recommendation to buy, sell, or hold any securities. Always consult with a qualified financial advisor before making investment decisions.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapseFour"
+                            aria-expanded="false"
+                            aria-controls="collapseFour"
+                        >
+                            Contact Us
+                        </button>
+                    </h2>
+                    <div
+                        id="collapseFour"
                         className="accordion-collapse collapse"
                         data-bs-parent="#accordionExample"
                     >
